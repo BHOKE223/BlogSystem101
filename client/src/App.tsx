221fb0ab75db0f6f91876ac561825d2619b4ad68
BHOKE223 <replicate@brenthoke.com>
2025-06-15 13:42:48 +0100

    Update App.tsx - incremental backup

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
+import NotFound from "@/pages/not-found";
+import ImageTest from "@/pages/image-test";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/image-test" component={ImageTest} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
